f95c749ff4f2a7a172bd8feb5dbfe8f162257844
sylvester <sylvestermbah90@gmail.com>
2026-06-30 06:25:36 -0700

    Add the increase and dercrease functions

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,16 @@ import { useState } from "react"
 
 const ProductCard = ({ product, cart, setCart }) => {
   const [quantity, setQuantity] = useState(1);
+
+  function decreaseQuantity() {
+    if (quantity > 1) {
+        setQuantity(quantity - 1);
+    }
+  }
+
+  function increaseQuantity() {
+    setQuantity(quantity + 1)
+  }
   return (
     <div>ProductCard</div>
   )
