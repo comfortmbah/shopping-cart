487121e0970214ec447768c1a9894f9eaa557886
sylvester <sylvestermbah90@gmail.com>
2026-06-30 06:53:11 -0700

    Update

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -11,7 +11,7 @@ const RootLayout = () => {
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cart))
-  },[cart]);
+  }, [cart]);
 
   return (
     <div className='min-h-screen bg-slate-100'>
