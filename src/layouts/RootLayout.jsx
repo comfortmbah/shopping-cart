8a246a664384b333e4baaceca977373acc48e06e
sylvester <sylvestermbah90@gmail.com>
2026-06-30 02:50:25 -0700

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,7 +7,7 @@ const RootLayout = () => {
 
   return (
     <div className='min-h-screen bg-slate-100'>
-      <Navbar />
+      <Navbar cart={cart} />
       <main className='max-w-7xl mx-auto px-4 py-8'>
         <Outlet context={{ cart, setCart }} />
       </main>
