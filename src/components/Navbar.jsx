5353c8f9561c770519cf58d035534fda25047b53
sylvester <sylvestermbah90@gmail.com>
2026-06-30 02:55:07 -0700

    Calculate total cart items using reducer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
 
 const Navbar = ({ cart }) => {
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
   return (
     <div>Navbar</div>
   )
