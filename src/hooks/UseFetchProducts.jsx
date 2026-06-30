fc491fae0a61d3a8d2b3b17aca4769029b4f1205
sylvester <sylvestermbah90@gmail.com>
2026-06-30 05:27:20 -0700

    Create reusable UseFetchProducts hook

--- src/hooks/UseFetchProducts.jsx
+++ src/hooks/UseFetchProducts.jsx
@@ -4,9 +4,29 @@ const UseFetchProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  return (
-    <div>UseFetchProducts</div>
-  )
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        } 
+
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
 }
 
 export default UseFetchProducts
