e8de631b60be8d65fc5a7506aeec0412b7b356fb
sylvester <sylvestermbah90@gmail.com>
2026-06-30 06:50:33 -0700

    Save cart state to localStorage

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom'
+import { jsxs } from 'react/jsx-runtime';
 
 const RootLayout = () => {
   const [cart, setCart] = useState(() => {
     const savedCart = localStorage.getItem('cart');
     return savedCart ? JSON.parse(savedCart) : [];
   });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  },[cart]);
 
   return (
     <div className='min-h-screen bg-slate-100'>
