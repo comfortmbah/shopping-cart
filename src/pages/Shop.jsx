116e84cd0433fe1f1093dc4595319fdca405deb3
sylvester <sylvestermbah90@gmail.com>
2026-06-30 06:00:23 -0700

    Use custom hook to fetch products

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,6 +5,7 @@ import { useOutletContext } from 'react-router-dom'
 
 const Shop = () => {
   const { cart, setCart } = useOutletContext();
+  const { products, loading, error } = UseFetchProducts();
   return (
     <div>Shop</div>
   )
