19705a69a7573d9e7cfcfeb39bf7026ea59a8689
sylvester <sylvestermbah90@gmail.com>
2026-06-30 05:54:11 -0700

    Add custom data fetch hook and useOutletContext

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,7 @@
 import { Loading } from '../components/Loading'
 import { Error } from '../components/Error'
+import UseFetchProducts from '../hooks/UseFetchProducts'
+import { useOutletContext } from 'react-router-dom'
 
 const Shop = () => {
   return (
