93eb7d13552e3ad90dff9d99273ac17d80a81b2c
sylvester <sylvestermbah90@gmail.com>
2026-06-30 05:18:22 -0700

    Initialize products state

--- src/hooks/UseFetchProducts.jsx
+++ src/hooks/UseFetchProducts.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react'
 
 const UseFetchProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   return (
     <div>UseFetchProducts</div>
   )
