5524680eb721752e0413e48676ff5a39a762b1d0
sylvester <sylvestermbah90@gmail.com>
2026-07-01 04:07:22 -0700

    Add logic that checks if the cart is empty

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -30,7 +30,20 @@ const Cart = () => {
 
   const totalItem = cart.reduce((total, item) => total + item.quantity, 0);
   const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
-  
+
+  if (cart.length === 0) {
+    return (
+      <section className="flex min-h-[70vh] flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-slate-800">
+          Your Cart is Empty
+        </h1>
+        <p className="mt-4 text-slate-400">
+          Add some products from the shop
+        </p>
+      </section>
+    )
+  }
+
   return (
     <div>
 
