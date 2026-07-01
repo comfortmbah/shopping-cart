f62b41db7e7aaddf0e53fc91b01c1f91aeb7dc40
sylvester <sylvestermbah90@gmail.com>
2026-07-01 03:42:16 -0700

    Add increase quantity function

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,16 @@ import { useOutletContext } from "react-router-dom"
 
 const Cart = () => {
   const { cart, setCart } = useOutletContext();
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      return item.id === id
+        ? { ...item, quantity: item.quantity + 1}
+        : item
+    });
+
+    setCart(updatedCart);
+  }
   return (
     <div>
 
