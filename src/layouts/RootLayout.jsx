7e1787cce4c1893406a1b6d1a8f94726971374ba
sylvester <sylvestermbah90@gmail.com>
2026-07-28 11:04:47 -0700

    Refactor RootLayout to use CartContext

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer';
 
 
 const RootLayout = () => {
-  const [cart, setCart] = useState(() => {
-    const savedCart = localStorage.getItem('cart');
-    return savedCart ? JSON.parse(savedCart) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  }, [cart]); 
+  
 
   return (
     <div className='min-h-screen bg-slate-100'>
-      <Navbar cart={cart} />
+      <Navbar />
       <main className='max-w-7xl mx-auto px-4 py-8'>
-        <Outlet context={{ cart, setCart }} />
+        <Outlet />
       </main>
       <Footer />
     </div>
