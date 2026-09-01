b53c94d2f2fda32211605283056ea9edc7310e94
sylvester <sylvestermbah90@gmail.com>
2026-09-01 08:33:11 -0700

    Add product details that fetch product by ID

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -4,3 +4,10 @@ export const getProductsList = async (req, res) => {
   const products = await response.json();
   res.json(products);
 }
+
+export const getProductDetails = async (req, res) => {
+  const { id } = req.params;
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const products = await response.json();
+  res.json(products);
+}
