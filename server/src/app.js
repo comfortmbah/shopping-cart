dc1d52a3ec32e4eb1041ce3db92be45a1321433e
sylvester <sylvestermbah90@gmail.com>
2026-09-01 07:48:03 -0700

    Enable CORS for React frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import productRouter from "./routes/productRoutes.js"
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
