ed8570f111b8068aa4ab00d06eab60c33caff705
sylvester <sylvestermbah90@gmail.com>
2026-09-02 01:09:05 -0700

    Add 404 middleware for unknown routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import productRouter from "./routes/productRoutes.js"
 import { errorHandler } from "./middleware/errorHandler.js";
+import { notFound } from "./middleware/notFound.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRouter);
+
+app.use(notFound);
 
 app.use(errorHandler);
 
