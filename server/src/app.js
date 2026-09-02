4a42625d963d046fd7fb45a46710a3cbbe681fa7
sylvester <sylvestermbah90@gmail.com>
2026-09-02 00:58:53 -0700

    Add centralized Express error handling middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import productRouter from "./routes/productRoutes.js"
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRouter);
+
+app.use(errorHandler);
 
 export default app;
